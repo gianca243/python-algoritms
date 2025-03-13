072db5d35650492d7955b6925c1cb76e86e1dc7d
gianca243 <gianca243@gmail.com>
2025-03-12 20:43:49 -0500

    chore: Longest common prefix added

---
+++ Longest Common Prefix/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        result = strs[0]
+        for index in range(1,len(strs)):
+            limit = min(len(result), len(strs[index]))
+            cent = 0
+            while cent < limit and result[cent] == strs[index][cent]:
+                cent += 1
+            result = strs[index][:cent]
+        return result
